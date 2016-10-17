753af012e1e319b66889b736aa04210eb875705c
Ian McLeod <imcleod@redhat.com>
2016-10-17 14:55:44 -0500

    Wrong wget option - whoops

--- get_images.sh
+++ get_images.sh
@@ -3,19 +3,19 @@
 # Uncomment only one input image here
 
 # Kali Linux image for veyron/rockchip chromebooks- for kernel and modules
-wget -f input-kernel.img.xz https://images.offensive-security.com/arm-images/kali-2.1.2-veyron.img.xz
+wget -O input-kernel.img.xz https://images.offensive-security.com/arm-images/kali-2.1.2-veyron.img.xz
 
 # Kali Linux image for samsung/nyan/tegra chromebooks - for kernel and modules
-#wget -f input-kernel.img.xz https://images.offensive-security.com/arm-images/kali-2.1.2-acer.img.xz
+#wget -O input-kernel.img.xz https://images.offensive-security.com/arm-images/kali-2.1.2-acer.img.xz
 
 # Kali Linux image for Peach chromebooks - for kernel and modules
-#wget -f input-kernel.img.xz https://images.offensive-security.com/arm-images/kali-2.1.2-exynos.img.xz
+#wget -O input-kernel.img.xz https://images.offensive-security.com/arm-images/kali-2.1.2-exynos.img.xz
 
 
 # F25 Beta - 32 bit arm - XFCE
 # Find a local mirror if possible
 # This should work with other spin images - YMMV
-wget -f input-fedora.img.xz http://mirror.sfo12.us.leaseweb.net/fedora/linux/development/25/Spins/armhfp/images/Fedora-Xfce-armhfp-25-20161014.n.0-sda.raw.xz
+wget -O input-fedora.img.xz http://mirror.sfo12.us.leaseweb.net/fedora/linux/development/25/Spins/armhfp/images/Fedora-Xfce-armhfp-25-20161014.n.0-sda.raw.xz
 
 xz -d input-kernel.img.xz
 xz -d input-fedora.img.xz
